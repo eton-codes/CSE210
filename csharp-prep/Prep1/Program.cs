f57184963bb95e5922efa34fddb6f16b8c157980
Eton Williams <etonwilliams98@gmail.com>
2026-04-25 23:10:58 -0600

    W1 p1-2 finish

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string one = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string two = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {two}, {one} {two}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string grade = Console.ReadLine();
+        int integer = int.Parse(grade);
+
+        string alphabet = "";
+
+        if (integer >= 90)
+        {
+            alphabet = "A";
+        }
+        else if (integer >= 80)
+        {
+            alphabet = "B";
+        }
+        else if (integer >= 70)
+        {
+            alphabet = "C";
+        }
+        else if (integer >= 60)
+        {
+            alphabet = "D";
+        }
+        else
+        {
+            alphabet = "F";
+        }
+
+
+        Console.WriteLine($"Your grade is: {alphabet}");
+
+        if (integer >= 70)
+        {
+            Console.WriteLine("You passed!");
+
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+
     }
 }
